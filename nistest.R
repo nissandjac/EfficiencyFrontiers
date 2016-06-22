348e8f1daafa443b53ce2ee4458d1460624c7dec
Nis Sand Jacobsen <nissandjac@gmail.com>
2016-06-22 12:38:11 -0700

    Doing sick plots

---
+++ nistest.R
@@ -0,0 +1,16 @@
+### Test FIle
+setwd("C:/Users/Nis/Documents/GitHub/EfficiencyFrontiers")
+p <- rnorm(200, mean = 10, sd = 1)
+l <- seq(1,100,length.out = 200)
+
+
+
+pdf('test.pdf')
+plot(p,l)
+dev.off()
+
+l2 <- l + rnorm(200,mean = 0,sd = 0.2)
+
+pdf('test2.pdf')
+plot(l,l2)
+dev.off()
